de9761ba7c14428b1b51216aa5c1216160b9d214
peejaywk <peejaywk@gmail.com>
2021-08-10 09:30:26 +0000

    Modify the settings.py file to get the secret key from environment variables.

--- vinylrack/settings.py
+++ vinylrack/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1_&_28krx%4ymf=2#$%_)a*swtk4x%rf0r&05he$fha4$mzijw'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
