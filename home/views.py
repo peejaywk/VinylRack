79cf3f13db1c6b208f6c16536c19c5aaf001884e
peejaywk <peejaywk@gmail.com>
2021-08-23 09:13:57 +0000

    Add products to the home page. Add product details template and view

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """
     A view to return the index page.
     """
-    return render(request, 'home/index.html')
+    products = Product.objects.order_by('artist').all()
+    template = 'home/index.html'
+    context = {
+        'products': products,
+    }
+    return render(request, template, context)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Product
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+    template = 'products/product_detail.html'
+    context = {
+        'product': product,
+    }
+
+    return render(request, template, context)
