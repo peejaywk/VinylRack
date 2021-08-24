1e02dd1711fa52dbc840624ea279146d8456ed97
peejaywk <peejaywk@gmail.com>
2021-08-24 10:19:55 +0000

    Add products page and implement the search function.

--- products/views.py
+++ products/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 
-from .models import Product
+from .models import Product, Artist
+
+
+def all_products(request):
+    """ A view to show all products """
+
+    products = Product.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            # Search performed on artist, description and album title.
+            queries = Q(artist__name__icontains=query) | Q(description__icontains=query) | Q(album_title__icontains=query)
+            print(queries)
+            print(products)
+            products = products.filter(queries)
+
+    template = 'products/products.html'
+    context = {
+        'products': products,
+        'search_term': query,
+    }
+
+    return render(request, template, context)
 
 
 def product_detail(request, product_id):
