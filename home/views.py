6bd6b43f9729217c354ab95ba03225fd0278e89f
peejaywk <peejaywk@gmail.com>
2021-08-29 07:23:10 +0000

    Update product details page to include quantity input and add to bag button

--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ def index(request):
     """
     A view to return the index page.
     """
-    products = Product.objects.order_by('artist').all()
+    products = Product.objects.order_by('-date_added').all()[:8]
     template = 'home/index.html'
     context = {
         'products': products,
