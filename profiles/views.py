f595353742b27f8c351f264e087b0cb4f339271c
peejaywk <peejaywk@gmail.com>
2021-08-31 16:41:31 +0000

    Add order history to the profile page.

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
 
 def profile(request):
     """ Display the user's profile """
@@ -27,6 +28,26 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    # template = 'checkout/checkout_success.html'
+    # Placeholder until the stripe payment app has been implemented and the
+    # checkout_success page implemented.
+    template = 'index.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
