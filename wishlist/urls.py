8a4b839c496c9e2f97ca29da6a914f12edc6efbc
peejaywk <peejaywk@gmail.com>
2021-09-11 14:33:55 +0000

    Create view to add item to wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.wishlist, name='wishlist'),
+    path('add/<int:product_id>', views.add_to_wishlist, name='add_to_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 from .models import Wishlist
+from products.models import Product
 
 
 @login_required
 def wishlist(request):
-    """ View to display a users wishlist """
-    
+    """ View to display a user's wishlist """
+
     # Get the current logged in users wishlist
     # If it doesn't exist then create one.
     wishlist = Wishlist.objects.get_or_create(user=request.user)
@@ -17,3 +20,20 @@ def wishlist(request):
         'wishlist': wishlist,
     }
     return render(request, template, context)
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    """ A view to add a product to a user's wishlist """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    # Get the current logged in users wishlist
+    # If it doesn't exist then create one.
+    wishlist = Wishlist.objects.get_or_create(user=request.user)
+    wishlist = wishlist[0]
+
+    wishlist.products.add(product)
+    messages.success(request, 'Product added to your wishlist')
+
+    return redirect(reverse('wishlist'))
