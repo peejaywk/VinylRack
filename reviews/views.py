92a735e96556d916b747999e076bec62566eb434
peejaywk <peejaywk@gmail.com>
2021-09-13 08:29:35 +0000

    Add view, url and template to display list of reviews for a user

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Review
 
-# Create your views here.
+
+def list_reviews(request):
+    """ View to display a list of reviews """
+
+    reviews = Review.objects.filter(user=request.user)
+    template = 'reviews/reviews.html'
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, template, context)
