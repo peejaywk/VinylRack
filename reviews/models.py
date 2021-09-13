abc9fee23ee61a2f29789566df5ff892312001b3
peejaywk <peejaywk@gmail.com>
2021-09-13 08:00:14 +0000

    Create basic model struct for user reviews and add to admin console

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from products.models import Product
 
-# Create your models here.
+
+class Review(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        null=False, blank=False, related_name='review')
+
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE,
+        null=False, blank=False, related_name='product')
+
+    created_on = models.DateTimeField(default=timezone.now)
+
+    review_title = models.CharField(max_length=254, null=False, blank=False)
+    review_content = models.TextField(max_length=1200, null=False, blank=False, default='')
+
+    def __str__(self):
+        return self.review_title
