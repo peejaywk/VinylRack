7b2f6d2fa34715cd7f576871345e653a78bb1db7
peejaywk <peejaywk@gmail.com>
2021-09-13 10:45:13 +0000

    Create view/template to add a product review

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.list_reviews, name='list_reviews'),
+    path('add/<int:product_id>', views.add_review, name='add_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+
 from .models import Review
+from .forms import ReviewForm
+from products.models import Product
 
 
 def list_reviews(request):
@@ -18,4 +22,35 @@ def list_reviews(request):
     context = {
         'reviews': reviews,
     }
+    return render(request, template, context)
+
+
+def add_review(request, product_id):
+    """ A view to add a new product review """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review_title = form.cleaned_data['review_title']
+            review_content = form.cleaned_data['review_content']
+            Review.objects.create(
+                user=request.user,
+                product=product,
+                review_title=review_title,
+                review_content=review_content
+            )
+            messages.success(request, 'Successfully added review!')
+            return redirect(reverse('list_reviews'))
+        else:
+            messages.error(request, 'Failed to add review. Please ensure the form is valid.')
+    else:
+        form = ReviewForm()
+
+    context = {
+        'form': form,
+        'product': product,
+    }
+    template = 'reviews/add_review.html'
     return render(request, template, context)
