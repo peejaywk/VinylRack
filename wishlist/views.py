40271e4420b8887bf44e68e52b1bf03c1947eab1
peejaywk <peejaywk@gmail.com>
2021-09-13 06:11:41 +0000

    Update the wishlist add view to check if an item is already in a users wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .models import Wishlist
+from .models import Wishlist, WishlistItem
 from products.models import Product
 
 
@@ -31,14 +31,19 @@ def add_to_wishlist(request, product_id):
     # Get the url the request has been made from so the
     # user can be redirected to that page
     previous_url = request.META.get('HTTP_REFERER')
-    print(previous_url)
+
     # Get the current logged in users wishlist
     # If it doesn't exist then create one.
     wishlist = Wishlist.objects.get_or_create(user=request.user)
     wishlist = wishlist[0]
 
-    wishlist.products.add(product)
-    messages.success(request, 'Product added to your wishlist')
+    in_wishlist =  WishlistItem.objects.filter(wishlist=wishlist, product=product).exists()
+
+    if in_wishlist:
+        messages.info(request, 'Product is already in your wishlist')
+    else:
+        wishlist.products.add(product)
+        messages.success(request, 'Product added to your wishlist')
 
     return redirect(previous_url)
 
