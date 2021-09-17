09800efa91c20f2fc7a1e27d942451b563f3c70e
peejaywk <peejaywk@gmail.com>
2021-09-17 10:14:35 +0000

    Create contact form and update view and template

---
+++ contact/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(required=True)
+    contact_email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        placeholders = {
+            'name': 'Full Name',
+            'contact_email': 'Email Address',
+            'message': 'Message',
+        }
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            self.fields[field].label = placeholders[field]           
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 
 def contact(request):
+    """ Manage the contact form page """
 
-    context = {}
+    form = ContactForm(request.POST or None)
+    context = {
+        'form': form,
+    }
     template = 'contact/contact.html'
     return render(request, template, context)
