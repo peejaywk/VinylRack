45e28e6ec7f0aa80a6930bbe9a17f0c88e2b717e
peejaywk <peejaywk@gmail.com>
2021-09-17 09:44:42 +0000

    Add view, template and url for the contact page

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def contact(request):
+
+    context = {}
+    template = 'contact/contact.html'
+    return render(request, template, context)
