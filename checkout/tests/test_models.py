e579a00a1dbd94b76417fa3720bc24feaf12153e
peejaywk <peejaywk@gmail.com>
2021-10-19 13:16:44 +0000

    Add unit tests to the checkout app.

--- checkout/tests/test_models.py
+++ checkout/tests/test_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from checkout.models import Order, OrderLineItem
 from products.models import Product, Artist, Genre, Recordlabel
+from django.conf import settings
 
 
 class TestAppModel(TestCase):
@@ -33,9 +34,9 @@ class TestAppModel(TestCase):
             record_label=self.recordlabel,
             album_title='Legacy',
             description='Double album released in 2017',
-            price='19.99',
+            price=19.99,
             on_sale=False,
-            discount_percent='0',
+            discount_percent=0,
             image=None,
             image_url='/media/123.jpg',
             media_condition='VG',
@@ -44,8 +45,8 @@ class TestAppModel(TestCase):
         )
         self.product.save()
 
-    def test_checkout_str(self):
-        order = Order(
+        # Create an order
+        self.order = Order(
             order_number='1234567890',
             full_name='John Smith',
             email='john.smith@email.com',
@@ -56,21 +57,70 @@ class TestAppModel(TestCase):
             county='Yorkshire',
             postcode='YH106DQ',
             country='UK',
-            delivery_cost='0',
-            order_total='0',
-            grand_total='0',
+            delivery_cost=0,
+            order_total=0,
+            grand_total=0,
             original_bag='1234',
             stripe_pid='123456',
         )
-        order.save()
+        self.order.save()
 
-        self.assertEqual(str(order), '1234567890')
+    def test_order_str(self):
+        self.assertEqual(str(self.order), '1234567890')
 
+    def test_orderlineitem_str(self):
         orderlineitem = OrderLineItem(
-            order=order,
+            order=self.order,
+            product=self.product,
+            quantity=1,
+        )
+        orderlineitem.save()
+        self.assertEqual(str(orderlineitem), 'SKU p1234567 on order 1234567890')
+
+    def test_orderlineitem_save(self):
+        """
+        Tests that the lineitem_total is updated when the save method
+        is called.
+        """
+        orderlineitem = OrderLineItem(
+            order=self.order,
             product=self.product,
             quantity=1,
         )
         orderlineitem.save()
 
-        self.assertEqual(str(orderlineitem), 'SKU p1234567 on order 1234567890')
+        total = orderlineitem.quantity * self.product.price
+        self.assertEqual(orderlineitem.lineitem_total, total)
+
+        # Change quantity to 2 and call save method
+        # Check the lineitem_total has been updated.
+
+        orderlineitem = OrderLineItem(
+            order=self.order,
+            product=self.product,
+            quantity=2,
+        )
+        orderlineitem.save()
+
+        total = orderlineitem.quantity * self.product.price
+        self.assertEqual(orderlineitem.lineitem_total, total)
+
+    def test_order_update_total(self):
+        """
+        Test the update_total method in the order model.
+        """
+  
+        orderlineitem = OrderLineItem(
+            order=self.order,
+            product=self.product,
+            quantity=2,
+        )
+        orderlineitem.save()
+
+        delivery_cost = (
+            self.order.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100)      
+        grand_total = self.order.order_total + delivery_cost
+        self.order.update_total()
+
+        self.assertEqual(self.order.delivery_cost, delivery_cost)
+        self.assertEqual(self.order.grand_total, grand_total)
